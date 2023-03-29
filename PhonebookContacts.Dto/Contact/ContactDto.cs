252d52c4b95a51578e686878bb05bd1ddf37b97e
Jiří Dvořák <jiridvorak1998@seznam.cz>
2023-03-29 15:34:27 +0200

    Dto validations attributes

--- PhonebookContacts.Dto/Contact/ContactDto.cs
+++ PhonebookContacts.Dto/Contact/ContactDto.cs
@@ -7,20 +7,20 @@ namespace PhonebookContacts.Dto.Contact
         [Required]
         public int Id { get; set; }
 
-        [Required]
+        [Required, StringLength(100, MinimumLength = 1, ErrorMessage = "First name can not be empty.")]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required]
+        [Required, StringLength(100, MinimumLength = 1, ErrorMessage = "Last name can not be empty.")]
         public string LastName { get; set; } = string.Empty;
 
         [Required]
         public DateTime BirthDate { get; set; }
 
-        [Required, EmailAddress]
+        [Required, EmailAddress(ErrorMessage = "Email must be valid."), StringLength(100, MinimumLength = 1, ErrorMessage = "Email can not be empty.")]
         public string Email { get; set; } = string.Empty;
 
-        [Phone]
-        public string PhoneNumber { get; set; } = string.Empty;
+        [Required, Phone(ErrorMessage = "Phone number must be valid.")]
+        public string? PhoneNumber { get; set; } = string.Empty;
 
         public bool IsActive { get; set; }
     }
